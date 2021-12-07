8a415f79917966a315d3cab356470b8a9bf9ae18
Virginia <virginiapiazza19@gmail.com>
2021-12-07 20:00:46 -0300

    Se completan el login y logout Servlet

--- src/main/java/controladorLogin/LoginServlet.java
+++ src/main/java/controladorLogin/LoginServlet.java
@@ -1,7 +1,45 @@
 package controladorLogin;
 
+
+import java.io.IOException;
 import jakarta.servlet.http.HttpServlet;
 
-public class LoginServlet extends HttpServlet {
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import modelo.Usuario;
+import servicios.LoginServicio;
 
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+	private static final long serialVersionUID = 8308079314140233763L;
+	private LoginServicio loginService;
+
+	@Override
+	public void init() throws ServletException {
+		super.init();
+		loginService = new LoginServicio();
+	}
+	
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    	String nombre = req.getParameter("nombre");
+    	String password = req.getParameter("password");
+    	
+    	Usuario usuario = loginService.login(nombre, password);
+    	
+    	if (!usuario.isNull()) {
+    		req.getSession().setAttribute("usuario", usuario);
+    		resp.sendRedirect("index.jsp");    		
+       	} else {
+    		req.setAttribute("flash", "Nombre de usuario o contraseña incorrectos");
+    		
+    		RequestDispatcher dispatcher = getServletContext()
+      		      .getRequestDispatcher("/login.jsp");
+      		    dispatcher.forward(req, resp);
+    	}
+    }
 }
+
