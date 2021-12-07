a5728e6c74cec6bf4a50701c780150dd082f14c8
Virginia <virginiapiazza19@gmail.com>
2021-12-07 20:02:18 -0300

    Se agregan los setters, dejo comentado todo lo relacionado a password

--- src/main/java/modelo/Usuario.java
+++ src/main/java/modelo/Usuario.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
+import utilidades.Crypt;
+
 public class Usuario {
 
 	public int id;
@@ -14,12 +16,13 @@ public class Usuario {
 	protected Itinerario itinerario;
 	protected List<Producto> nuevosProductos;
 
-	public Usuario(int id, String nombre, int presupuesto, double tiempoDisponible, String atraccionPreferida) {
+	public Usuario(int id, String nombre, int presupuesto, double tiempoDisponible, String atraccionPreferida/*, String password*/) {
 		this.id = id;
 		this.nombre = nombre;
 		this.presupuesto = presupuesto;
 		this.tiempoDisponible = tiempoDisponible;
 		this.atraccionPreferida = atraccionPreferida;
+		//this.password = password;
 		nuevosProductos = new ArrayList<Producto>();
 	}
 	
@@ -47,6 +50,26 @@ public class Usuario {
 		return this.itinerario.productos;
 	}
 	
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public void setPresupuesto(int presupuesto) {
+		this.presupuesto = presupuesto;
+	}
+
+	public void setTiempoDisponible(double tiempoDisponible) {
+		this.tiempoDisponible = tiempoDisponible;
+	}
+
+	public void setAtraccionPreferida(String atraccionPreferida) {
+		this.atraccionPreferida = atraccionPreferida;
+	}
+
 	public void setItinerario(List<Producto> productos) {
 		this.itinerario = new Itinerario(productos);
 	}
@@ -68,7 +91,7 @@ public class Usuario {
 	@Override
 	public String toString() {
 		return "\nUSUARIO \n Nombre: " + nombre + " | Presupuesto: " + presupuesto + " | Tiempo Disponible: " + tiempoDisponible
-				+ "hs. | Atraccion Preferida: " + atraccionPreferida;
+				+ "hs. | Atraccion Preferida: " + atraccionPreferida /* + "Contraseña: *****"*/;
 	}
 
 	@Override
@@ -92,4 +115,27 @@ public class Usuario {
 				&& Objects.equals(itinerario, other.itinerario) 
 				&& Objects.equals(atraccionPreferida, other.atraccionPreferida);
 	}	
+	
+	public boolean isNull() {
+		return false;
+	}
+	
+	private boolean puedeComprar(Producto p) {
+		if (this.getPresupuesto() < p.getCosto() || this.getTiempoDisponible() < p.getDuracion()) {
+			return false;
+		}
+		return true;
+	}
+	
+	/*private String password;
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = Crypt.hash(password);
+	}
+	public boolean checkPassword(String password) {
+		return Crypt.match(password, this.password);
+	}
+	*/
 }
