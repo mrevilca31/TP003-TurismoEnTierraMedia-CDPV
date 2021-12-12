8069938d5a0f293f93947436ae0b4246a8c39bb0
Virginia <virginiapiazza19@gmail.com>
2021-12-12 18:25:39 -0300

    Se eliminan las excepciones

--- src/main/java/excepciones/AtraccionException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class AtraccionException extends Exception {
-	public AtraccionException (String mensaje) {
-		super(mensaje);
-	}
-}
-
--- src/main/java/excepciones/UsuarioException.java
+++
@@ -1,9 +0,0 @@
-package excepciones;
-
-@SuppressWarnings("serial")
-public class UsuarioException extends Exception {
-
-	public UsuarioException(String mensaje) {
-		super(mensaje);
-	}
-}
