543243c0a7a94a1c2d9c5f7c8c21f19b635aabcf
Virginia <virginiapiazza19@gmail.com>
2021-12-12 13:19:11 -0300

    Se agrega main para probar que funcionen las conexiones de clase

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,19 @@
+package main;
+
+import java.util.List;
+
+import modelo.Usuario;
+import persistencia.UsuarioDAO;
+import persistenciaGeneral.DAOFactory;
+import servicios.LoginServicio;
+import servicios.UsuarioServicio;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		UsuarioServicio uservice = new UsuarioServicio();
+		System.out.println(uservice.find(1));
+		
+	}
+}
