665100935ebff0fbdedcc107ad62321c6417cade
Virginia <virginiapiazza19@gmail.com>
2021-12-15 21:36:52 -0300

    ItinerarioDAO

--- src/main/java/persistencia/ItinerarioDAO.java
+++ src/main/java/persistencia/ItinerarioDAO.java
@@ -7,7 +7,9 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import modelo.Atraccion;
 import modelo.Producto;
+import modelo.Promocion;
 import modelo.Usuario;
 import persistenciaGeneral.ConexionProvider;
 import persistenciaGeneral.MissingDataException;
