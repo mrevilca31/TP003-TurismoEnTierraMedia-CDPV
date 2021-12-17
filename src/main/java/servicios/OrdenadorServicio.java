8e63f02cfbe9e247feedd62c065ea96b3ce56cd9
Virginia <virginiapiazza19@gmail.com>
2021-12-17 00:12:44 -0300

    Cambios en el orenador de servicios

--- src/main/java/servicios/OrdenadorServicio.java
+++ src/main/java/servicios/OrdenadorServicio.java
@@ -15,15 +15,15 @@ public class OrdenadorServicio {
 	public List<Producto> crearListaOrdenada(String nombre) {
 		Usuario usuario = DAOFactory.getUsuarioDAO().findByNombre(nombre);
 		List<Atraccion> atraccionDAO = DAOFactory.getAtraccionDAO().findAll();
-		List<Promocion> promocionDAO = DAOFactory.getPromocionDAO().findAll();
+		// List<Promocion> promocionDAO = DAOFactory.getPromocionDAO().findAll();
 
 		List<Producto> ofertas = new ArrayList<Producto>();
 		for (Atraccion atraccion : atraccionDAO) {
 			ofertas.add(atraccion);
 		}
-		for (Promocion promo : promocionDAO) {
-			ofertas.add(promo);
-		}
+		/*
+		 * for (Promocion promo : promocionDAO) { ofertas.add(promo); }
+		 */
 		ofertas.sort(new ComparadorPorTipoAtraccion(usuario.getAtraccionPreferida()));
 
 		return ofertas;
