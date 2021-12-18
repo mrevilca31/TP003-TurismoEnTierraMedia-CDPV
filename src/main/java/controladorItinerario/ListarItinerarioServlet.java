354c769b83cb1ec87a6c27eaf32d764031c1a393
Virginia <virginiapiazza19@gmail.com>
2021-12-18 12:47:11 -0300

    Se eliminan imports no usados

--- src/main/java/controladorItinerario/ListarItinerarioServlet.java
+++ src/main/java/controladorItinerario/ListarItinerarioServlet.java
@@ -10,8 +10,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import modelo.Atraccion;
-import modelo.Itinerario;
 import modelo.Producto;
 import modelo.Usuario;
 import servicios.ItinerarioServicio;
