8737b95eb4d81c6de81df347d2fe2894f023f440
Virginia <virginiapiazza19@gmail.com>
2021-12-18 23:43:58 -0300

    Chequea si no es nula la lista

--- src/main/java/servicios/ItinerarioServicio.java
+++ src/main/java/servicios/ItinerarioServicio.java
@@ -13,10 +13,11 @@ public class ItinerarioServicio {
 
 	public List<Producto> find(Usuario usuario) {
 		List<Producto> productos = DAOFactory.getItinerarioDAO().findByNombre(usuario);
-		for(Producto comprasRealizadas:productos ) {
-			usuario.itinerario.add(comprasRealizadas);	
-		}
-		
-		return usuario.getItinerario();
+		if(productos != null) {
+			for(Producto comprasRealizadas:productos ) {
+				usuario.itinerario.add(comprasRealizadas);	
+			}	
+		}		
+		return usuario.itinerario;
 	} 
 }
